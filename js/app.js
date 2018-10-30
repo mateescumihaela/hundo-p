e161ce87700a4a7095f65852151d6777a9da9ac2
Mihaela Mateescu <mihaelamateescu@Air-van-Mihaela.home>
2018-10-30 14:58:44 +0000

    declared js variables and added event listener for button click

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const appKey = "f24f40b1c24505685fce3b8acd0fcffc";
+
+let searchButton = document.getElementById("search-btn");
+let searchInput = document.getElementById("search-txt");
+let cityName = document.getElementById("city-name");
+let icon = document.getElementById("icon");
+let temperature = document.getElementById("temp");
+
+searchButton.addEventListener("click", findWeatherDetails);
+searchInput.addEventListener("keyup", enterPressed);
+
+function enterPressed(event) {
+  if (event.key === "Enter") {
+    findWeatherDetails();
+  }
+}
