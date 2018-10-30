6e2b0b86d7c60dbca314758baf4caf5109a8e10e
Mihaela Mateescu <mihaelamateescu@Air-van-Mihaela.home>
2018-10-30 15:06:09 +0000

    Update js/app.js

--- js/app.js
+++ js/app.js
@@ -7,10 +7,37 @@ let icon = document.getElementById("icon");
 let temperature = document.getElementById("temp");
 
 searchButton.addEventListener("click", findWeatherDetails);
-searchInput.addEventListener("keyup", enterPressed);
+searchInput.addEventListener("keyup", pressedEnter);
 
-function enterPressed(event) {
+function pressedEnter(event) {
   if (event.key === "Enter") {
     findWeatherDetails();
   }
+}
+function findWeatherDetails() {
+  if (searchInput.value === "") {
+  
+  }else {
+    let searchLink = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid="+appKey;
+   httpRequestAsync(searchLink, response);
+  }
+ }
+
+function response(response) {
+  let jsonObject = JSON.parse(response);
+  cityName.innerHTML = jsonObject.name;
+  icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
+  temperature.innerHTML = parseInt(jsonObject.main.temp - 273) + "°";
+}
+
+function httpRequestAsync(url, callback)
+{
+  console.log("hello");
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = () => { 
+        if (httpRequest.readyState == 4 && httpRequest.status == 200)
+            callback(httpRequest.responseText);
+    }
+    httpRequest.open("GET", url, true);
+    httpRequest.send();
 }
