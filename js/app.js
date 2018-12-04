a84886bf68de4d49c135bfef2058c0d10a956cff
mateescumihaela <miha.mateescu@gmail.com>
2018-12-04 15:07:24 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
+function openTab(tabName) {
+  let i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
 const appKey = "f24f40b1c24505685fce3b8acd0fcffc";
-
 let searchButton = document.getElementById("search-btn");
 let searchInput = document.getElementById("search-txt");
 let cityName = document.getElementById("city-name");
@@ -9,25 +16,29 @@ let temperature = document.getElementById("temp");
 searchButton.addEventListener("click", findWeatherDetails);
 searchInput.addEventListener("keyup", pressedEnter);
 
+
 function pressedEnter(event) {
   if (event.key === "Enter") {
     findWeatherDetails();
   }
 }
+
 function findWeatherDetails() {
   if (searchInput.value === "") {
   
   }else {
     let searchLink = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid="+appKey;
-   httpRequestAsync(searchLink, response);
+   httpRequestAsync(searchLink, theResponse);
   }
  }
-function response(response) {
+
+function theResponse(response) {
   let jsonObject = JSON.parse(response);
   cityName.innerHTML = jsonObject.name;
   icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
   temperature.innerHTML = parseInt(jsonObject.main.temp - 273) + "°";
 }
+
 function httpRequestAsync(url, callback)
 {
   console.log("hello");
@@ -36,6 +47,6 @@ function httpRequestAsync(url, callback)
         if (httpRequest.readyState == 4 && httpRequest.status == 200)
             callback(httpRequest.responseText);
     }
-    httpRequest.open("GET", url, true);
+    httpRequest.open("GET", url, true); 
     httpRequest.send();
 }
